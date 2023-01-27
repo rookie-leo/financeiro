11b6d2f19685e5904db4c7e161476548d9318426
rookie-leo <proleonarpa@gmail.com>
2023-01-27 17:17:08 -0300

    [ config ] Configurando projeto para teste de classes controllers.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into receita(descricao, valor, data_entrada) values ('Pagamento', 1445, '2022-12-12');
+insert into receita(descricao, valor, data_entrada) values ('Receita aluguel', 300, '2022-12-12');
+insert into receita(descricao, valor, data_entrada) values ('Receita poupança', 800, '2022-11-12');
+insert into receita(descricao, valor, data_entrada) values ('Pagamento', 1445, '2023-01-12');
+
+insert into despesa(descricao, valor, data_entrada, categoria) values ('Conta de Agua', 58.90, '2022-12-12', 'Moradia');
+insert into despesa(descricao, valor, data_entrada, categoria) values ('Fatura Cartão de Crádito banco xyz', 596, '2022-12-12', 'Lazer');
+insert into despesa(descricao, valor, data_entrada, categoria) values ('Fatura Cartão de Crádito banco abc', 135.42, '2022-11-12', 'Imprevistos');
+insert into despesa(descricao, valor, data_entrada, categoria) values ('Conta de Luz', 1445, '2023-01-12', 'Moradia');
